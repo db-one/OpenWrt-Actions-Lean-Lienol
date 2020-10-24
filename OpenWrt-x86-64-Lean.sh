47cf58ac0a045edd45cc6036b0caf877e061b007
db-one <20243226+kang-mk@users.noreply.github.com>
2020-10-24 10:20:55 +0800

    禁用微信推送

--- OpenWrt-x86-64-Lean.sh
+++ OpenWrt-x86-64-Lean.sh
@@ -138,8 +138,8 @@ EOF
 # 第三方插件选择:
 cat >> .config <<EOF
 CONFIG_PACKAGE_luci-app-oaf=y #应用过滤
-# CONFIG_PACKAGE_luci-app-openclash=y #OpenClash
-CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
+# CONFIG_PACKAGE_luci-app-openclash=y #OpenClash客户端
+# CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
 CONFIG_PACKAGE_luci-app-eqos=y #IP限速
 # CONFIG_PACKAGE_luci-app-smartdns=y #smartdns服务器
 # CONFIG_PACKAGE_luci-app-adguardhome=y #ADguardhome
